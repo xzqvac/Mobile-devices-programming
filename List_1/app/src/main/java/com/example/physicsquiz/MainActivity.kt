ad241724d8a4277677ad61b3bd8a4c6905f4e73f
xzqvac <kawkaradoslaw@gmail.com>
2022-10-18 22:06:43 +0200

    Add CheatActivity

--- List_1/app/src/main/java/com/example/physicsquiz/MainActivity.kt
+++ List_1/app/src/main/java/com/example/physicsquiz/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.physicsquiz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    fun startCheatingActivity(view: View){
+        val intent = Intent(this, CheatActivity::class.java)
+        startActivity(intent)
+    }
+
 }
