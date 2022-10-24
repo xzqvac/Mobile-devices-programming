a66ec2e2e02afec06d170d5dd0707fa1123c4bf1
xzqvac <kawkaradoslaw@gmail.com>
2022-10-24 20:28:50 +0200

    Add changing activities & correct couting points

--- List_1/app/src/main/java/com/example/physicsquiz/CheatActivity.kt
+++ List_1/app/src/main/java/com/example/physicsquiz/CheatActivity.kt
@@ -2,10 +2,28 @@ package com.example.physicsquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class CheatActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.cheat_activity)
+
+        createBackButon()
+
+
+        val answer = intent.getStringExtra(EXTRA_MESSAGE)
+        findViewById<TextView>(R.id.txt_cheat_answer).apply {
+            text = answer
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+
+    private fun createBackButon(){
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 }
--- List_1/app/src/main/java/com/example/physicsquiz/MainActivity.kt
+++ List_1/app/src/main/java/com/example/physicsquiz/MainActivity.kt
@@ -6,63 +6,82 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
+
+
+const val EXTRA_MESSAGE = "com.example.physicsquiz.MESSAGE"
 
 class MainActivity : AppCompatActivity() {
-    private var currentPosition = 1
+    private var currentPosition: Int = 1
     private val questions: List<Question> = Questions.questions
-    private var selectedAnswerPosition = 0
-    private var points = 0
-    private var correctAnswer = 0
-    private var amountOfFraud = 0
+    private var points: Int = 0
+    private var correctAnswers: Int = 0
+    private var amountOfFraud: Int = 0
 
-    private val questionText: TextView by lazy { findViewById(R.id.txt_vw_question)}
-    private val answerTrue: Button by lazy { findViewById(R.id.btn_true)}
-    private val answerFalse: Button by lazy { findViewById(R.id.btn_false)}
-    private val answerCheat: Button by lazy { findViewById(R.id.btn_cheat)}
+    private val answerTrue: Button by lazy { findViewById(R.id.btn_true) }
+    private val answerFalse: Button by lazy { findViewById(R.id.btn_false) }
+    private val answerCheat: Button by lazy { findViewById(R.id.btn_cheat) }
+
+    private val questionText: TextView by lazy { findViewById(R.id.txt_vw_question) }
+    private val cheatAnswerText: TextView by lazy { findViewById(R.id.txt_cheat_answer) }
 
 
-
-    override fun onCreate(savedInstanceState: Bundle?)
-    {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState != null) {
+            currentPosition = savedInstanceState.getInt("currentPosition_state")
+            points = savedInstanceState.getInt("points_state")
+            correctAnswers = savedInstanceState.getInt("CorrectAnswers_state")
+            amountOfFraud = savedInstanceState.getInt("Frauds_state")
+        }
+
         setQuestion()
 
         answerTrue.setOnClickListener {
-            if (selectedAnswerPosition == 0) {
-                currentPosition++
-                if (currentPosition <= questions.size)
-                    setQuestion()
-                else
-                    Toast.makeText(this, "Quiz completed", Toast.LENGTH_SHORT).show()
+            val question = questions[currentPosition - 1]
+            if (question.correctAnswer == 1) {
+                points += 10
+                correctAnswers
             }
-            else {
-                val question = questions[currentPosition - 1]
-                if (question.correctAnswer == 1)
-                    points++
-            }
+            currentPosition++
+            if (currentPosition <= questions.size)
+                setQuestion()
+            else
+                questionText.text = points.toString()
         }
 
-        answerFalse.setOnClickListener{
-            if (selectedAnswerPosition == 0) {
-                currentPosition++
-                if (currentPosition <= questions.size)
-                    setQuestion()
-                else
-                    Toast.makeText(this, "Quiz completed", Toast.LENGTH_SHORT).show()
+        answerFalse.setOnClickListener {
+            val question = questions[currentPosition - 1]
+            if (question.correctAnswer == 0) {
+                points += 10
+                correctAnswers++
             }
-            else {
-                val question = questions[currentPosition - 1]
-                if (question.correctAnswer == 0)
-                    points++
-            }
+            currentPosition++
+            if (currentPosition <= questions.size)
+                setQuestion()
+            else
+                questionText.text = points.toString()
         }
+    }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("points_state", points)
+        outState.putInt("currentPosition_state", currentPosition)
+        outState.putInt("CorrectAnswers_state", correctAnswers)
+        outState.putInt("Frauds_state", amountOfFraud)
 
     }
-    fun startCheatingActivity(view: View){
-        val intent = Intent(this, CheatActivity::class.java)
+
+    fun startCheatingActivity(view: View) {
+        amountOfFraud++
+        points -= 15
+        val question = questions[currentPosition - 1]
+        val answer = if (question.correctAnswer == 1) "TRUE" else "FALSE"
+        val intent = Intent(this, CheatActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, answer)
+        }
         startActivity(intent)
     }
 
@@ -70,10 +89,8 @@ class MainActivity : AppCompatActivity() {
         val question = questions[currentPosition - 1]
         questionText.text = question.question
     }
-
-
-
 }
+
 
 class Question(
     val question: String,
@@ -82,7 +99,7 @@ class Question(
 
 object Questions {
     val questions: List<Question> = listOf(
-        Question("QUESTION 1", 1),
+        Question("QUESTION 1", 0),
         Question("QUESTION 2", 0),
-        Question("QUESTION 3", 1))
+        Question("QUESTION 3", 0))
 }
