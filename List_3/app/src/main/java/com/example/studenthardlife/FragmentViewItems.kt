319d4ac1c49428b9e205a6e0c3715070354a84af
xzqvac <kawkaradoslaw@gmail.com>
2022-12-10 18:29:46 +0100

    Add functionality of add buton

--- List_3/app/src/main/java/com/example/studenthardlife/FragmentViewItems.kt
+++ List_3/app/src/main/java/com/example/studenthardlife/FragmentViewItems.kt
@@ -1,6 +1,5 @@
 package com.example.studenthardlife
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -12,14 +11,10 @@ import com.example.studenthardlife.databinding.FragmentViewItemsBinding
 
 
 class FragmentViewItems : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
-    //private val binding by lazy { FragmentViewItemsBinding.inflate(layoutInflater)}
+    private val dbHandler by lazy { DBHandler(requireContext()) }
     private var _binding: FragmentViewItemsBinding? = null
     private val binding get() = _binding!!
-    // error i think
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,13 +22,13 @@ class FragmentViewItems : Fragment() {
         saveInstanceState: Bundle?
     ): View? {
         _binding = FragmentViewItemsBinding.inflate(inflater, container, false)
-        return binding.root
-        //return inflater.inflate(R.layout.fragment_view_items, container, false)
-
+        //return binding.root
+        return inflater.inflate(R.layout.fragment_view_items, container, false)
 
     }
 
     override fun onDestroyView() {
+        dbHandler.close()
         super.onDestroyView()
         _binding = null
     }
@@ -41,10 +36,24 @@ class FragmentViewItems : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val recyclerView: RecyclerView = view.findViewById(R.id.tasksRecyclerView)
-        val dbHandler by lazy { DBHandler(requireContext()) }
         binding.tasksRecyclerView.apply {
-                layoutManager = LinearLayoutManager(context)
-                adapter = TaskAdapter(dbHandler, context)
+                //layoutManager = LinearLayoutManager(context)
+                //adapter = TaskAdapter(dbHandler, context)
+            recyclerView.layoutManager = LinearLayoutManager(context)
+            recyclerView.adapter = TaskAdapter(dbHandler, context)
         }
-    }
+        binding.addTaskButton.setOnClickListener {
+            val name = binding.editSubjectName.text.toString()
+            val index = binding.editTasksDescription.text.toString()
+
+            if (name.isNotEmpty() && index.isNotEmpty()){
+                dbHandler.addTask(Task(name, index.toInt()))
+                binding.editSubjectName.text.clear()
+                binding.editTasksDescription.text.clear()
+            }
+        }
+        binding.tasksRecyclerView.adapter?.notifyItemInserted(dbHandler.getTasks().size)
+
+        }
+
 }
