6de16fd31c2fdf99bfcf2c241167ce5ac57dc8a0
xzqvac <kawkaradoslaw@gmail.com>
2023-02-05 22:29:42 +0100

    feat(FetchData): add fun to check the result of correct implement API client

--- Semester-project/app/src/main/java/com/example/footballstats/MainActivity.kt
+++ Semester-project/app/src/main/java/com/example/footballstats/MainActivity.kt
@@ -9,12 +9,17 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.footballstats.theme.footballStatsTheme
+import com.example.footballstats.viewmodel.liveFixtures.LiveFixturesModel
+import com.example.footballstats.viewmodel.state.FixturesState
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,8 +27,21 @@ class MainActivity : AppCompatActivity() {
             footballStatsTheme {
                 Column(modifier = Modifier.padding(10.dp)) {
                     TopAppBar()
+                    FetchData()
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun FetchData(liveFixturesModel: LiveFixturesModel = viewModel()) {
+    Column {
+        when(val state = liveFixturesModel.liveFixturesState.collectAsState().value) {
+            is FixturesState.Empty -> Text(text = "No data avaliabe")
+            is FixturesState.Loading -> Text(text = "Loading data")
+            is FixturesState.Success -> Text(text = "Success")
+            is FixturesState.Error -> Text(text = state.message)
         }
     }
 }
