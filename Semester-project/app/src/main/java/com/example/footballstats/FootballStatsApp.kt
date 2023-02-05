429fda2b0aed50fc3f9d35dbdeec774b1a6b545f
xzqvac <kawkaradoslaw@gmail.com>
2023-02-05 22:44:12 +0100

    chore: add class expended application class to generate dagger2 components

--- Semester-project/app/src/main/java/com/example/footballstats/FootballStatsApp.kt
+++ Semester-project/app/src/main/java/com/example/footballstats/FootballStatsApp.kt
@@ -1,4 +1,7 @@
 package com.example.footballstats
 
-class FootballStatsApp {
-}
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class FootballStatsApp: Application()
