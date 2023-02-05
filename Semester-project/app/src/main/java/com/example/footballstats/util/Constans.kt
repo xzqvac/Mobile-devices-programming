7ac76abc983c0142245e754a7ec3ef104dcf4a42
xzqvac <kawkaradoslaw@gmail.com>
2023-02-05 17:06:53 +0100

    chore(Constans.kt): add constans for connect to API

---
+++ Semester-project/app/src/main/java/com/example/footballstats/util/Constans.kt
@@ -0,0 +1,9 @@
+package com.example.footballstats.util
+
+const val BASE_URL = "https://app.sportdataapi.com/api/v1"
+
+const val GET_FIXTURES = "/soccer/matches"
+
+const val API_KEY = "216cb8c0-a4dc-11ed-9cda-d5f155d54079"
+
+const val SEASOND_ID = "3161"
