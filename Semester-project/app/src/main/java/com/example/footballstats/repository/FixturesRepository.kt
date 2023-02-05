144eadf30da202a86ad567d193fcb43fa705a0fe
xzqvac <kawkaradoslaw@gmail.com>
2023-02-05 18:42:38 +0100

    feat(FixturesRepository: add class with functions to store fetched data

---
+++ Semester-project/app/src/main/java/com/example/footballstats/repository/FixturesRepository.kt
@@ -0,0 +1,11 @@
+package com.example.footballstats.repository
+
+import com.example.footballstats.data.remote.APIService
+import com.example.footballstats.data.remote.models.Data
+import javax.inject.Inject
+
+class FixturesRepository @Inject constructor(private val sportDataAPIService: APIService){
+    suspend fun getLiveFixtures(): List<Data> = sportDataAPIService.getLiveFixtures().data
+
+    suspend fun getFixtures(): List<Data> = sportDataAPIService.getFixtures().data
+}
