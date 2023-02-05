85be247874d2bea451a9c558170fd55adc3e2542
xzqvac <kawkaradoslaw@gmail.com>
2023-02-05 16:17:27 +0100

    feat(MainActivity.kt): add top bar

--- Semester-project/app/src/main/java/com/example/footballstats/MainActivity.kt
+++ Semester-project/app/src/main/java/com/example/footballstats/MainActivity.kt
@@ -1,11 +1,48 @@
 package com.example.footballstats
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.footballstats.theme.footballStatsTheme
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            footballStatsTheme {
+                Column(modifier = Modifier.padding(10.dp)) {
+                    TopAppBar()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TopAppBar(){
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        IconButton(onClick = { /*TODO*/ }) {
+            Icon(imageVector = Icons.Default.Refresh, contentDescription = "Refresh Icon")
+        }
+
+        Text(text = "FootballStats", style = MaterialTheme.typography.h4)
+
+        IconButton(onClick = { /*TODO*/ }){
+            Icon(imageVector = Icons.Default.Star, contentDescription = "Favourite Icon")
+        }
     }
 }
